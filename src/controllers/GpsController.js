06e91555002a4978d61ded6935289204b8a859e0
Victormiluu <167822048+Victormiluu@users.noreply.github.com>
2025-04-28 19:36:53 -0300

    Delete src directory

--- src/controllers/GpsController.js
+++
@@ -1,7 +0,0 @@
-// /backend/src/controllers/GpsController.js
-exports.updateLocation = (req, res) => {
-    const { latitude, longitude } = req.body;
-    // Lógica para processar os dados do GPS e atualizar a localização do veículo no mapa
-    res.json({ success: true, message: 'Localização atualizada com sucesso' });
-  };
-  
